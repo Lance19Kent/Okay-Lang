151541e60618b3ca766ea17004b57113707cdafe
Lance Kent <lancemagollado46@gmail.com>
2025-12-30 10:43:33 +0800

    header added

--- okay-lang-app/app/layout.tsx
+++ okay-lang-app/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Quicksand } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+});
+
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  weight:["300", "400", "500", "600", "700"],
 });
 
 const geistMono = Geist_Mono({
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${quicksand.className} antialiased`}
       >
         {children}
       </body>
--- okay-lang-app/app/page.tsx
+++ okay-lang-app/app/page.tsx
@@ -1,65 +1,23 @@
-import Image from "next/image";
 
-export default function Home() {
+export default function LandingPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="min-h-screen bg-linear-to-b from-[#FFFCEC] from-50% to-[#FFEA73] to-77% flex flex-col items-center">
+      <header className="w-full h-12.5 p-3 flex items-center justify-between">
+        <div className="flex items-center">
+          <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19.3333 16.3573V14.6667C19.3333 13.9594 19.6143 13.2811 20.1144 12.781C20.6145 12.281 21.2928 12 22 12H23C23.0884 12 23.1732 12.0351 23.2357 12.0976C23.2982 12.1601 23.3333 12.2449 23.3333 12.3333V13.3333C23.3333 14.0406 23.0524 14.7189 22.5523 15.219C22.0522 15.719 21.3739 16 20.6667 16C19.9594 16 19.2811 16.281 18.781 16.781C18.281 17.2811 18 17.9594 18 18.6667C18 20 18.6667 20.6667 18.6667 22C18.6667 22.7212 18.4327 23.423 18 24" stroke="#AEC6CF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M12.6667 16C13.1619 15.6286 13.7508 15.4024 14.3673 15.3468C14.9838 15.2912 15.6037 15.4084 16.1574 15.6853C16.7111 15.9621 17.1767 16.3877 17.5022 16.9142C17.8276 17.4408 18 18.0476 18 18.6667C17.5048 19.0381 16.9159 19.2643 16.2993 19.3199C15.6828 19.3755 15.063 19.2583 14.5093 18.9814C13.9556 18.7046 13.4899 18.279 13.1645 17.7524C12.839 17.2259 12.6667 16.6191 12.6667 16Z" stroke="#AEC6CF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M13.3333 24H22.6667" stroke="#AEC6CF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          <h1 className="text-[#333333]">Okay Lang</h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#333333" stroke-opacity="0.8" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M12 16V12" stroke="#333333" stroke-opacity="0.8" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M12 8H12.01" stroke="#333333" stroke-opacity="0.8" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+
+      </header>
+    </main>
   );
 }
