a198ee1c2c8a674e60b98d48c873eb2ec75360ad
Lance Kent <lancemagollado46@gmail.com>
2025-12-30 12:22:38 +0800

    Landing Page done

--- okay-lang-app/app/page.tsx
+++ okay-lang-app/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { useState } from "react";
+
 
 export default function LandingPage() {
+
+  const [key, setKey] = useState("");
+
+  const handleSubmit = (e: React.FormEvent)=>{
+    e.preventDefault();
+    console.log("Key:", key);
+  }
+
   return (
-    <main className="min-h-screen bg-linear-to-b from-[#FFFCEC] from-50% to-[#FFEA73] to-77% flex flex-col items-center">
+    <main className="min-h-screen bg-linear-to-b from-[#FFFCEC] from-50% to-[#FFEA73] to-77% flex flex-col items-center gap-18">
       {/* Logo Icons */}
       <header className="w-full h-12.5 p-3 flex items-center justify-between">
         <div className="flex items-center">
@@ -12,8 +23,36 @@ export default function LandingPage() {
       </header>
 
       {/* Main Body */}
-      <div className="">
+      <div className=" w-full flex flex-col items-center px-3">
+        <h1 className="text-[36px] text-[#333333]">Hinga muna.</h1>
+        <p className="text-[12px] text-[#333333] opacity-50 text-center">A gentle space where people can check in with themselves without pressure, explanation, or judgment.</p>
+        <img src="/svg/landing-vector.svg"></img>
+      </div>
 
+      {/* Buttons */}
+      <div className="w-full px-3 flex flex-col gap-8">
+        <button className="w-full bg-[#F9F9F9] py-5 text-[#333333] rounded-[124px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] active:shadow-none active:translate-y-0.5 transition-all">
+          Start new journey
+        </button>
+        
+        <form onSubmit={handleSubmit}>
+          <p className="text-[12px] text-[#333333] opacity-60">— or continue with existing key —</p>
+          <div className="flex gap-5 px-2">
+            <input 
+            type="text" 
+            placeholder="Paste Key here" 
+            value={key} 
+            onChange={(e) => setKey(e.target.value)} 
+            className="w-full bg-white rounded-xl p-3">
+            </input>
+            <button type="submit" className="p-3 rounded-full bg-[#AEC6CF]">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M18 8L22 12L18 16" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M2 12H22" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </button>
+          </div>
+        </form>
       </div>
     </main>
   );
