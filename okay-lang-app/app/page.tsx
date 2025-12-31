5c71bb270690122f151ca7e802854c692d8dc93f
Lance Kent <lancemagollado46@gmail.com>
2025-12-31 19:18:25 +0800

    Modified Page

--- okay-lang-app/app/page.tsx
+++ okay-lang-app/app/page.tsx
@@ -46,10 +46,7 @@ export default function LandingPage() {
             className="w-full bg-white rounded-xl p-3">
             </input>
             <button type="submit" className="p-3 rounded-full bg-[#AEC6CF]">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M18 8L22 12L18 16" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                  <path d="M2 12H22" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
+              <img src="/svg/arrow.svg" alt="" />
             </button>
           </div>
         </form>
